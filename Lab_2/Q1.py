9e4e817d50fa740d3bbcb45507ee993698a46065
lortiz-gonzalez <lortiz-gonzalez@ucmerced.edu>
2023-02-07 12:26:06 -0800

    Added question 1 and 2

---
+++ Lab_2/Q1.py
@@ -0,0 +1,28 @@
+#Lab2 Q1
+
+import numpy as np
+
+def main():
+    #a)
+    arr = np.arange(2,10).reshape(4,2)
+    print(arr)
+    #b) NEED TO CORRECT
+    # arr2 = [n for n in range(0,2)].reshape(8,8)
+    # print(arr2)
+    #c)
+    List = [10, 20, 10, 30, 20, 40, 20, 20, 10, 30, 0, 50, 10]
+    print("Unique values:",np.unique(List))
+    #d)
+    List = np.array([6, 75, 9, 82, 36, 42, 59, 3, 52, 1, 32, 68, 93, 4, 27, 85, 0, -3, 57])
+    print("Values greater than 37:",List[np.where((List > 37))])
+    #e)
+    change = (9/5)
+    change2 = 32
+    List = np.array([0, 12, 45.21 ,34, 99.91]) * change + change2
+    # x = List[1]
+    # far = x*(9/5)+32
+    print(List)
+
+
+if __name__ == '__main__':
+    main()
