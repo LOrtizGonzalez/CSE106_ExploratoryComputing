113e1d2c67a28aeee899f49a81ab7a079488fb1f
lortiz-gonzalez <lortiz-gonzalez@ucmerced.edu>
2023-02-28 10:59:50 -0800

    Made new css. Need to fix issue minor issue after hitting '=' and resetting the strings

--- Lab_4/myScript.js
+++ Lab_4/myScript.js
@@ -1,7 +1,6 @@
 var calc_str = "", str = "", last_action = "";
 var dec = false; //flag checks to see if decimal has been added already
 var clear = false; //to clear current input or clear the whole operation.
-var number = 0;
 
 function buttonClicked(val) {
     var len = str.length;
@@ -25,7 +24,7 @@ function arithmeticButtonClicked(sign) {
     document.getElementById("calc_display").innerHTML = str;
     if (last_action.length != 0){ //if size if >1, means that an arithmetic symbol is already in the string and the num string can be inputted
         last_action += str;       //before moving on.
-        console.log(last_action);
+        //console.log(last_action); //FOR TESTING PURPOSES
     }
     if (sign == '=') {
         if (str.length < 1) { //when you click '=' back-to-back, will continue to do the last operation
@@ -35,70 +34,35 @@ function arithmeticButtonClicked(sign) {
             calc_str += str; 
             str = "";
         }
-        console.log("last command",last_action);
-        console.log("current calcs",calc_str);
+        console.log("last command",last_action); //FOR TESTING PURPOSES
+        console.log("current calcs",calc_str); //FOR TESTING PURPOSES
         var solution = eval(calc_str);
         document.getElementById("calc_display").innerHTML = solution;
     }
     else {
         last_action = sign;
-        console.log(last_action);
+        //console.log(last_action); //FOR TESTING PURPOSES
         calc_str += str;
         str = "";
         document.getElementById("calc_display").innerHTML = eval(calc_str); //-----
         calc_str += sign;
-        console.log(calc_str);
+        console.log(calc_str); //FOR TESTING PURPOSES
     }
 }
 
-    function clearFunction() {
-        if (clear == true) {
-            calc_str = "";
-            str = "";
-            last_action = "";
-            clear = false;
-            document.getElementById("calc_display").innerHTML = 0;
-        }
-        else {
-            str = "";
-            clear = true;
-            document.getElementById("calc_display").innerHTML = 0;
-        }
-
+function clearFunction() {
+    if (clear == true) {
+        calc_str = "";
+        str = "";
+        last_action = "";
+        clear = false;
+        console.log(str, "second clear", calc_str, "---", last_action);
+        document.getElementById("calc_display").innerHTML = 0;
     }
-
-
-    //str = "";
-    // if (sign == '+') {
-    //     // calc_str += str;
-    //     str = "";
-    //     calc_str += sign;
-    //     console.log(calc_str);
-    // }
-    // else if (sign == '-') {
-    //     str = "";
-    //     calc_str += sign;
-    //     console.log(calc_str);
-    // }
-    // else if (sign == 'x') {
-    //     str = "";
-    //     calc_str += '*';
-    //     console.log(calc_str);
-    // }
-    // else if (sign == '/') {
-    //     calc_str += sign;
-    //     console.log(calc_str);
-    // }
-    // else {
-    //     // calc_str += str;
-    //     console.log(calc_str);
-    //     console.log(str);
-    //     var solution = eval(calc_str);
-    //     console.log(solution);
-    //     document.getElementById("calc_display").innerHTML = solution;
-    // }
-    
-
-
-// var equations = "12+1*2-2/.5";
-// console.log(eval(equations));
+    else {
+        str = "";
+        clear = true;
+        console.log(str, "first clear ", calc_str)
+        document.getElementById("calc_display").innerHTML = 0;
+    }
+}
